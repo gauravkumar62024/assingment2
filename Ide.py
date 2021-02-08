abf1687773f878dd3eea6ceecd9944baa133ee4d
GAURAV KUMAR <78220135+gauravkumar62024@users.noreply.github.com>
2021-02-08 06:11:41 -0800

    Update Ide.py

--- Ide.py
+++ Ide.py
@@ -6,7 +6,7 @@ print('REQUIRED SAMPLE SPACE WILL BE ',sample_space)
 lenght=len(sample_space)
 print("LET OUS ASSUME THAT THE OUTCOMES WILL BE TAILS APPERA FIRST")
 A={OT for OT in sample_space if OT[0]=='T'}
-print('THEN THE DESIRED OUTCOME BE A=',A)
+print('THEN THE DESIRED OUTCOME BE A=',A,'=4')
 def prob(X):
      return len(X)/len(sample_space)
 print('THEREFORE THE P(A)=',prob(A))
